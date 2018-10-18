549eeab64039e1d72f0e65cf70cac363eac2d71c
Santhosh Bonala <S530859@nwmissouri.edu>
2018-10-18 16:09:32 -0500

    added delpoyment configuration

--- ProjectNorthwestTheatre/frontend/src/config/config.js
+++ ProjectNorthwestTheatre/frontend/src/config/config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  url: 'http://ec2-18-191-243-115.us-east-2.compute.amazonaws.com:3000/admin',
-  userurl: 'http://ec2-18-191-243-115.us-east-2.compute.amazonaws.com:3000/user'
+const config = {
+  development: {
+    url: 'http://localhost:3000/admin',
+    userurl: 'http://localhost:3000/user'
+  },
+  production: {
+    url: 'http://ec2-18-191-243-115.us-east-2.compute.amazonaws.com:3000/admin',
+    userurl: 'http://ec2-18-191-243-115.us-east-2.compute.amazonaws.com:3000/user'
+  }
 }
+
+module.exports = config[process.env.NODE_ENV] | config.development
